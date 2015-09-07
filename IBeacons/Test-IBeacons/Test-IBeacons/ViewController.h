d6589a2975ce1034cc6bbe2b1eb9f9243ca485fb
Winner-Wolf <gabinnivert@hotmail.fr>
2015-09-07 14:38:54 +0200

    Init App IOS

---
+++ IBeacons/Test-IBeacons/Test-IBeacons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test-IBeacons
+//
+//  Created by Gabin NIVERT on 07/09/2015.
+//  Copyright (c) 2015 Gabin NIVERT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IBeacons/Test-IBeacons/Test-IBeaconsTests/Test_IBeaconsTests.m
@@ -0,0 +1,40 @@
+//
+//  Test_IBeaconsTests.m
+//  Test-IBeaconsTests
+//
+//  Created by Gabin NIVERT on 07/09/2015.
+//  Copyright (c) 2015 Gabin NIVERT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Test_IBeaconsTests : XCTestCase
+
+@end
+
+@implementation Test_IBeaconsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
