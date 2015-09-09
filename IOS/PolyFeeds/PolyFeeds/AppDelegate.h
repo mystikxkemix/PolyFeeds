6b59ea7a82a2bca0c154f3f2c0d86761c000ba5f
Winner-Wolf <gabinnivert@hotmail.fr>
2015-09-09 11:44:48 +0200

    Update 0.4 Xcode

---
+++ IOS/PolyFeeds/PolyFeeds/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PolyFeeds
+//
+//  Created by Gabin NIVERT on 07/09/2015.
+//  Copyright (c) 2015 Gabin NIVERT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
